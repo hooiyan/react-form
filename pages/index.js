d60910fdfa8069017b8a7be90ff8bc031dab9b28
hooiyan <90371582+hooiyan@users.noreply.github.com>
2022-09-16 20:12:59 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import AppForm from '@/components/AppForm'
 import Helmet from '@/components/Helmet'
 import TestingForm from '@/components/TestingForm'
+import { Input } from '@chakra-ui/react'
 
 export default function HomePage() {
   return (
     <>
       <Helmet title="React Form" />
       <AppForm />
-      <TestingForm />
+      {/* <TestingForm /> */}
+      <form>
+        <Input
+          list="food"
+          name="food"
+        />
+        <datalist id="food">
+          <option value="burger" />
+          <option value="french fries" />
+          <option value="spaghetti" />
+          <option value="pizza" />
+        </datalist>
+      </form>
     </>
   )
 }
