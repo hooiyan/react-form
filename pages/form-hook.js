8d2d7bddd85f6aaa241958d388aab1e274f40b6b
hooiyan <90371582+hooiyan@users.noreply.github.com>
2022-09-18 11:10:27 +0800

    update

--- pages/form-hook.js
+++ pages/form-hook.js
@@ -1,7 +1,111 @@
-import React from 'react'
+import { Box, Flex, Input, Select } from '@chakra-ui/react'
+
+import AppFormControl from '@/components/AppFormControl'
+import AppInput from '@/components/AppInput'
+import useForm from '@/lib/useForm'
+import Helmet from '@/components/Helmet'
+import { day, month, year } from '../utils'
 
 function ReactHooksFormPage() {
-  return <div>HookFormPage</div>
+  const { inputs, handleChange } = useForm({
+    firstname: 'Jenny',
+    surname: 'Kim',
+    loginMethod: '0167319898',
+  })
+
+  return (
+    <Box>
+      <Helmet title="React hooks" />
+      <Flex gap={4}>
+        <AppFormControl
+          isRequired
+          label="First name"
+        >
+          <Input
+            id="firstname"
+            name="firstname"
+            value={inputs.firstname}
+            onChange={handleChange}
+          />
+        </AppFormControl>
+
+        <AppFormControl
+          isRequired
+          label="Surname"
+        >
+          <Input
+            id="surname"
+            name="surname"
+            value={inputs.surname}
+            onChange={handleChange}
+          />
+        </AppFormControl>
+      </Flex>
+
+      <AppFormControl
+        isRequired
+        label="Mobile number or email address"
+      >
+        <Input
+          id="loginMethod"
+          name="loginMethod"
+          value={inputs.loginMethod}
+          onChange={handleChange}
+        />
+      </AppFormControl>
+
+      <AppFormControl
+        isRequired
+        label="Password"
+      >
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          value={inputs.password}
+          onChange={handleChange}
+        />
+      </AppFormControl>
+
+      <AppFormControl
+        isRequired
+        label="Date of birth"
+      >
+        <Flex gap={4}>
+          <Select>
+            {day.map((d) => (
+              <option
+                key={d}
+                value={d}
+              >
+                {d}
+              </option>
+            ))}
+          </Select>
+          <Select>
+            {month.map((m) => (
+              <option
+                key={m}
+                value={m}
+              >
+                {m}
+              </option>
+            ))}
+          </Select>
+          <Select>
+            {year.map((y) => (
+              <option
+                key={y}
+                value={y}
+              >
+                {y}
+              </option>
+            ))}
+          </Select>
+        </Flex>
+      </AppFormControl>
+    </Box>
+  )
 }
 
 export default ReactHooksFormPage
--- pages/form-no-js.js
+++ pages/form-no-js.js
@@ -1,48 +1,52 @@
+import Helmet from '@/components/Helmet'
 import styles from '@/styles/NoJSForm.module.css'
 
 function NoJSFormPage() {
   return (
-    <form
-      action="http://localhost:3000/api/login"
-      method="post"
-      className={styles.form}
-    >
-      <label htmlFor="username">Username</label>
-      <input
-        required
-        id="username"
-        name="username"
-        type="text"
-        placeholder="John Doe"
-        className={styles.input}
-      />
-      <br />
-      <label htmlFor="password">Password</label>
-      <input
-        required
-        id="password"
-        name="password"
-        type="password"
-        placeholder="******"
-        className={styles.input}
-      />
-      <br />
-
-      <div className={styles.btnGrp}>
+    <>
+      <Helmet title="Form without JS" />
+      <form
+        action="http://localhost:3000/api/login"
+        method="post"
+        className={styles.form}
+      >
+        <label htmlFor="username">Username</label>
         <input
-          type="submit"
-          value="Log in"
+          required
+          id="username"
+          name="username"
+          type="text"
+          placeholder="John Doe"
           className={styles.input}
         />
         <br />
-        <button
-          type="reset"
+        <label htmlFor="password">Password</label>
+        <input
+          required
+          id="password"
+          name="password"
+          type="password"
+          placeholder="******"
           className={styles.input}
-        >
-          Reset
-        </button>
-      </div>
-    </form>
+        />
+        <br />
+
+        <div className={styles.btnGrp}>
+          <input
+            type="submit"
+            value="Log in"
+            className={styles.input}
+          />
+          <br />
+          <button
+            type="reset"
+            className={styles.input}
+          >
+            Reset
+          </button>
+        </div>
+      </form>
+    </>
   )
 }
 
