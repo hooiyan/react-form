84c5e52be0a253acaaf8d3faeb53eea3802bf58a
hooiyan <90371582+hooiyan@users.noreply.github.com>
2022-09-18 16:13:33 +0800

    update

--- pages/form-hook.js
+++ pages/form-hook.js
@@ -1,4 +1,11 @@
-import { Box, Button, Flex, Input, Select } from '@chakra-ui/react'
+import {
+  Button,
+  Flex,
+  Input,
+  Radio,
+  RadioGroup,
+  Select,
+} from '@chakra-ui/react'
 
 import AppFormControl from '@/components/AppFormControl'
 import Helmet from '@/components/Helmet'
@@ -10,114 +17,154 @@ function ReactHooksFormPage() {
     firstname: 'Jenny',
     surname: 'Kim',
     loginMethod: '0167319898',
-    dob: { day: '10', month: 'Mar', year: '2000' },
+    password: '',
+    day: '10',
+    month: 'Mar',
+    year: '2000',
+    gender: 'female',
   })
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputs)
+  }
+
   return (
-    <Box>
+    <>
       <Helmet title="React hooks" />
-      <Flex gap={4}>
-        <AppFormControl
-          isRequired
-          label="First name"
-        >
-          <Input
-            id="firstname"
-            name="firstname"
-            value={inputs.firstname}
-            onChange={handleChange}
-          />
-        </AppFormControl>
-
-        <AppFormControl
-          isRequired
-          label="Surname"
-        >
-          <Input
-            id="surname"
-            name="surname"
-            value={inputs.surname}
-            onChange={handleChange}
-          />
-        </AppFormControl>
-      </Flex>
-
-      <AppFormControl
-        isRequired
-        label="Mobile number or email address"
-      >
-        <Input
-          id="loginMethod"
-          name="loginMethod"
-          value={inputs.loginMethod}
-          onChange={handleChange}
-        />
-      </AppFormControl>
-
-      <AppFormControl
-        isRequired
-        label="Password"
-      >
-        <Input
-          id="password"
-          name="password"
-          type="password"
-          value={inputs.password}
-          onChange={handleChange}
-        />
-      </AppFormControl>
-
-      <AppFormControl
-        isRequired
-        label="Date of birth"
-      >
+      <form onSubmit={(e) => handleSubmit(e)}>
         <Flex gap={4}>
-          <Select
-            value={inputs.dob.day}
-            onChange={handleChange}
+          <AppFormControl
+            isRequired
+            label="First name"
+            htmlFor="firstname"
           >
-            {day.map((d) => (
-              <option
-                key={d}
-                value={d}
-              >
-                {d}
-              </option>
-            ))}
-          </Select>
-          <Select
-            value={inputs.dob.month}
-            onChange={handleChange}
+            <Input
+              id="firstname"
+              name="firstname"
+              value={inputs.firstname}
+              onChange={handleChange}
+            />
+          </AppFormControl>
+
+          <AppFormControl
+            isRequired
+            label="Surname"
+            htmlFor="surname"
           >
-            {month.map((m) => (
-              <option
-                key={m}
-                value={m}
-              >
-                {m}
-              </option>
-            ))}
-          </Select>
-          <Select
-            value={inputs.dob.year}
-            onChange={handleChange}
-          >
-            {year.map((y) => (
-              <option
-                key={y}
-                value={y}
-              >
-                {y}
-              </option>
-            ))}
-          </Select>
+            <Input
+              id="surname"
+              name="surname"
+              value={inputs.surname}
+              onChange={handleChange}
+            />
+          </AppFormControl>
         </Flex>
-      </AppFormControl>
 
-      <AppFormControl label="Gender"></AppFormControl>
+        <AppFormControl
+          isRequired
+          label="Mobile number or email address"
+          htmlFor="loginMethod"
+        >
+          <Input
+            id="loginMethod"
+            name="loginMethod"
+            value={inputs.loginMethod}
+            onChange={handleChange}
+          />
+        </AppFormControl>
 
-      <Button colorScheme="green">Sign up</Button>
-    </Box>
+        <AppFormControl
+          isRequired
+          label="Password"
+          htmlFor="password"
+        >
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </AppFormControl>
+
+        <AppFormControl
+          isRequired
+          label="Date of birth"
+          htmlFor="dob"
+        >
+          <Flex gap={4}>
+            <Select
+              name="day"
+              value={inputs.day}
+              onChange={handleChange}
+            >
+              {day.map((d) => (
+                <option
+                  key={d}
+                  value={d}
+                >
+                  {d}
+                </option>
+              ))}
+            </Select>
+            <Select
+              name="month"
+              value={inputs.month}
+              onChange={handleChange}
+            >
+              {month.map((m) => (
+                <option
+                  key={m}
+                  value={m}
+                >
+                  {m}
+                </option>
+              ))}
+            </Select>
+            <Select
+              name="year"
+              value={inputs.year}
+              onChange={handleChange}
+            >
+              {year.map((y) => (
+                <option
+                  key={y}
+                  value={y}
+                >
+                  {y}
+                </option>
+              ))}
+            </Select>
+          </Flex>
+        </AppFormControl>
+
+        <AppFormControl
+          label="Gender"
+          htmlFor="gender"
+        >
+          <RadioGroup
+            id="gender"
+            name="gender"
+            value={inputs.gender}
+            onChange={(e) => console.log(e)}
+          >
+            <Flex justifyContent="space-between">
+              <Radio value="male">Male</Radio>
+              <Radio value="female">Female</Radio>
+              <Radio value="custom">Custom</Radio>
+            </Flex>
+          </RadioGroup>
+        </AppFormControl>
+
+        <Button
+          type="submit"
+          colorScheme="green"
+        >
+          Sign up
+        </Button>
+      </form>
+    </>
   )
 }
 
