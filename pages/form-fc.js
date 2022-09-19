575b802745dbef0b6814f8f7dc7c339e3827ecc1
hooiyan <90371582+hooiyan@users.noreply.github.com>
2022-09-19 20:06:57 +0800

    update

--- pages/form-fc.js
+++ pages/form-fc.js
@@ -1,5 +1,5 @@
 import { Button, Flex, Heading, Image, Input } from '@chakra-ui/react'
-import { Form, Formik } from 'formik'
+import { Field, Form, Formik } from 'formik'
 
 import AppFormControl from '@/components/AppFormControl'
 import Helmet from '@/components/Helmet'
@@ -40,9 +40,14 @@ function FormikComponentsFormPage() {
                 htmlFor="username"
                 label="Your name"
               >
-                <Input
+                {/* <Input
                   placeholder="First and last name"
                   {...getFieldProps('username')}
+                /> */}
+                <Field
+                  as={Input}
+                  name="username"
+                  placeholder="First and last name"
                 />
               </AppFormControl>
 
@@ -51,9 +56,14 @@ function FormikComponentsFormPage() {
                 htmlFor="loginMethod"
                 label="Mobile number or email"
               >
-                <Input
+                {/* <Input
                   placeholder="Mobile number or email"
                   {...getFieldProps('loginMethod')}
+                /> */}
+                <Field
+                  as={Input}
+                  name="loginMethod"
+                  placeholder="Mobile number or email"
                 />
               </AppFormControl>
 
@@ -62,12 +72,19 @@ function FormikComponentsFormPage() {
                 htmlFor="password"
                 label="Password"
               >
-                <Input
+                {/* <Input
                   isRequired
                   type="password"
                   placeholder="At least 6 characters"
                   minLength={6}
                   {...getFieldProps('password')}
+                /> */}
+                <Field
+                  as={Input}
+                  type="password"
+                  name="password"
+                  placeholder="At least 6 characters"
+                  minLength={6}
                 />
               </AppFormControl>
 
@@ -76,10 +93,16 @@ function FormikComponentsFormPage() {
                 htmlFor="confirmPassword"
                 label="Re-enter password"
               >
-                <Input
+                {/* <Input
                   type="password"
                   minLength={6}
                   {...getFieldProps('confirmPassword')}
+                /> */}
+                <Field
+                  as={Input}
+                  type="password"
+                  name="confirmPassword"
+                  minLength={6}
                 />
               </AppFormControl>
 
