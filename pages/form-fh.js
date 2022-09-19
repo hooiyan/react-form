cfe1a0e40715edf9b29519e738a9fac3d2eda6d5
hooiyan <90371582+hooiyan@users.noreply.github.com>
2022-09-19 21:32:41 +0800

    update

--- pages/form-fh.js
+++ pages/form-fh.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import { Button, Input } from '@chakra-ui/react'
+import { useFormik } from 'formik'
+
+import AppFormControl from '@/components/AppFormControl'
+import Helmet from '@/components/Helmet'
 
 function FormikHooksFormPage() {
-  return <div>FormikHooksPage</div>
+  const formik = useFormik({
+    initialValues: {
+      username: 'Anonymous',
+      loginMethod: '',
+      password: '',
+      confirmPassword: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
+  const { values, getFieldProps, handleSubmit } = formik
+
+  return (
+    <>
+      <Helmet title="Formik hooks" />
+      <form onSubmit={handleSubmit}>
+        <AppFormControl
+          htmlFor="username"
+          label="Username"
+        >
+          <Input
+            {...getFieldProps('username')}
+            value={values.username}
+          />
+        </AppFormControl>
+
+        <Button>Submit</Button>
+      </form>
+    </>
+  )
 }
 
 export default FormikHooksFormPage
